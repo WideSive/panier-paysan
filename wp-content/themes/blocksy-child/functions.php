78a2a19bb5195ecfb5422f58f05a822b0eb27f02
WideSive <paco.bousquet@protonmail.com>
2026-03-12 11:19:48 +0100

    add producteur page

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -6,7 +6,7 @@ function mon_theme_enfant_enqueue_styles() {
 
     wp_enqueue_style(
         'global-custom',
-        get_stylesheet_directory_uri() . '/assets/global.css',
+        get_stylesheet_directory_uri() . '/assets/css/global.css',
         array( 'child-style' ),
         '1.0.0'
     );
@@ -15,9 +15,27 @@ function mon_theme_enfant_enqueue_styles() {
     if ( is_product_category() ) {
         wp_enqueue_style(
             'producteurs-style',
-            get_stylesheet_directory_uri() . '/assets/producteurs.css',
+            get_stylesheet_directory_uri() . '/assets/css/producteurs.css',
             array( 'child-style' ),
             '1.0.0'
+        );
+    }
+
+        // Seulement sur les pages producteur (taxonomy)
+    if (is_tax('producteur')) {
+        wp_enqueue_style(
+            'page-producteur-style',
+            get_stylesheet_directory_uri() . '/assets/css/page-producteur.css',
+            array('child-style'),
+            '1.0.0'
+        );
+        
+        // Charger aussi les styles des cards produits
+        wp_enqueue_style(
+            'producteurs-style',
+            get_stylesheet_directory_uri() . '/assets/css/producteurs.css',
+            array('child-style'),
+            '1.0.1'
         );
     }
 
@@ -25,7 +43,7 @@ function mon_theme_enfant_enqueue_styles() {
     if ( is_front_page() ) {
         wp_enqueue_style(
             'homepage-style',
-            get_stylesheet_directory_uri() . '/assets/homepage.css',
+            get_stylesheet_directory_uri() . '/assets/css/homepage.css',
             array( 'child-style' ),
             '1.0.0'
         );
@@ -35,10 +53,17 @@ function mon_theme_enfant_enqueue_styles() {
     if ( is_woocommerce() ) {
         wp_enqueue_style(
             'woo-custom',
-            get_stylesheet_directory_uri() . '/assets/woocommerce.css',
+            get_stylesheet_directory_uri() . '/assets/css/woocommerce.css',
             array( 'child-style' ),
             '1.0.0'
         );
     }
 }
 add_action( 'wp_enqueue_scripts', 'mon_theme_enfant_enqueue_styles' );
+
+// Image de fallback personnalisée pour les produits.
+add_filter( 'woocommerce_placeholder_img_src', 'custom_woocommerce_placeholder' );
+
+function custom_woocommerce_placeholder( $src ) {
+	return get_stylesheet_directory_uri() . '/assets/images/product-placeholder.webp';
+}
